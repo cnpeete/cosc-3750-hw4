df03bba52bfe9fd283b5f689933aed8b3fc6b305
cnpeete <cpeete@uwyo.edu>
2025-02-19 11:02:20 -0700

    first

---
+++ wycat.c
@@ -0,0 +1,44 @@
+/*
+wycat.c
+Author: Cameron Peete
+Date: February 19, 2025
+
+COSC 3750, Homework 4
+
+This is a simple version of the cat utility
+*/
+
+#define BUFFER_SIZE 4096
+
+#include<stdio.h>
+
+int main(int argc, char **argv)
+{
+  char buffer[BUFFER_SIZE];
+  size_t size = 1;
+  size_t numElements = 1;
+  int count = 1;
+
+  if (argc == 1)
+  {
+    while (fread(buffer, size, numElements, stdin) > 0)
+    {
+      fwrite(buffer, size, numElements, stdout);
+    }
+  }
+  else
+  {
+    while (count < argc)
+    {
+      if (strcmp(argv[count], '-') == 0)
+      {
+        while (fread(buffer, size, numElements, stdin) > 0)
+        {
+          fwrite(buffer, size, numElements, stdout);
+        }
+      }
+
+    }
+  }
+  return 0;
+}
