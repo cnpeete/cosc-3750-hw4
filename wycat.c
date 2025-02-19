ccdc36e4384fd99fafd4a5bbc0f946a8fcd3e0b8
cnpeete <cpeete@uwyo.edu>
2025-02-19 11:39:52 -0700

    commit

--- wycat.c
+++ wycat.c
@@ -11,6 +11,7 @@ This is a simple version of the cat utility
 #define BUFFER_SIZE 4096
 
 #include<stdio.h>
+#include<string.h>
 
 int main(int argc, char **argv)
 {
@@ -30,14 +31,18 @@ int main(int argc, char **argv)
   {
     while (count < argc)
     {
-      if (strcmp(argv[count], '-') == 0)
+      if (strcmp(argv[count], "-") == 0)
       {
         while (fread(buffer, size, numElements, stdin) > 0)
         {
           fwrite(buffer, size, numElements, stdout);
         }
       }
-
+      else
+      {
+        printf("The input was: %s\n", argv[count]);
+      }
+      count++;
     }
   }
   return 0;
